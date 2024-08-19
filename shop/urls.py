bc0f05777c9b7ba41bae3c10530028c6324e40e7
Pradhuman Tamboli <pradhumantamboli12@gmail.com>
2024-08-19 12:54:39 +0530

    code refector

--- shop/urls.py
+++ shop/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path, include
-from .views import home, logout_view, about, shop   
+from .views import home, logout_view, about, shop, create_product, create_category, category_list_view, product_list_view
 
 urlpatterns = [
     path('', home, name='home'),
     path('about/', about, name='about'),
     path('shop/', shop, name='shop'),
+    path('create_product/', create_product, name='create_product'),
+    path('create_category/', create_category, name='create_category'),
+    path('category_list/', category_list_view, name='category_list'),
+    path('product_list/', product_list_view, name='product_list'),
     path('logout/', logout_view, name='logout'),
     path('accounts/', include('django.contrib.auth.urls')),
-    
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,23 +1,68 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-
+from django.db import DatabaseError
+from .forms import ProductForm, CategoryForm
 
 # Create your views here.
-# from django.views.generic import TemplateView
-
-# class HomePageView(TemplateView):
-#     template_name = 'shop/home.html'
 
 def home(request):
     return render(request, 'shop/home.html')
 
 def about(request):
     return render(request, 'shop/about.html')
-
+from .models import Product, Category
 @login_required
 def shop(request):
-    return render(request, 'shop/shop.html')
+    product = get_all_products()
+    data={
+        'product':product
+    }
+    return render(request, 'shop/shop.html',data)
 
 
 def logout_view(request):
     return render(request, 'registration/logout.html')
+
+def create_product(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('product_list')  # Redirect to a success page
+    else:
+        form = ProductForm()
+
+    categories = get_all_categories()
+    return render(request, 'shop/create_product.html', {'form': form, 'categories': categories})
+
+
+
+def create_category(request):
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('category_list')  # Redirect to a success page
+    else:
+        form = CategoryForm()
+    return render(request, 'shop/create_category.html', {'form': form})
+
+def get_all_products():
+    try:
+        return Product.objects.all()
+    except DatabaseError as e:
+        return Product.objects.none() 
+
+def get_all_categories():
+    try:
+        return Category.objects.all()
+    except DatabaseError as e:
+        return Category.objects.none() 
+
+def category_list_view(request):
+    categories = get_all_categories()
+    return render(request, 'shop/category_list.html', {'categories': categories})
+
+def product_list_view(request):
+    products = get_all_products()
+    return render(request, 'shop/product_list.html', {'products': products})
