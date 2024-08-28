b52e3c4ebcbce3f88547e29df1fc3658dd9ef1b5
Pradhuman Tamboli <pradhumantamboli12@gmail.com>
2024-08-28 19:38:53 +0530

    Remove exposed email credentials

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -162,7 +163,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'projecttest103@gmail.com'
-EMAIL_HOST_PASSWORD = 'zjkc urit thqj dejh'
-DEFAULT_FROM_EMAIL = 'projecttest103@gmail.com'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
 
